b1d481cc831912d76b4e5cfd77073151b9371351
Fachrurazzi <razzytirta@gmail.com>
2022-05-16 06:40:39 +0800

    Final & Constant Dart

---
+++ 02_final_const.dart
@@ -0,0 +1,9 @@
+void main() {
+  // const -> wajib langsung di isi valuenya
+  const int umur = 24;
+  print(umur);
+  // final -> boleh diisi valuenya setelahnya
+  final String name;
+  name = "Fachrurazzi";
+  print(name);
+}
