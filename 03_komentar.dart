48c8f804eb685f4e3418321bccae6bc86567420b
Fachrurazzi <razzytirta@gmail.com>
2022-05-16 06:48:10 +0800

    Komentar Dart

---
+++ 03_komentar.dart
@@ -0,0 +1,16 @@
+void main() {
+  // comment single line
+
+  /* 
+    comment multiline
+  */
+
+  /**
+   * comment 1
+   * comment 2
+   * comment 3
+   * comment 4
+   */
+
+  /// documentation comments
+}
